8ac515c2a56b5a4106285004f6f5873674c6ba2e
Black-Garlic <hn1226@naver.com>
2024-01-10 01:19:46 +0900

    해산물 음식 목록 정렬 추가중

--- src/components/page/Fish/List/FishListFilter.tsx
+++ src/components/page/Fish/List/FishListFilter.tsx
@@ -8,21 +8,47 @@ import { getRegionColor } from "@libs/regionUtil.ts";
 import { getTimeColor } from "@libs/timeUtil.ts";
 import { Fish } from "@typings/Fish.ts";
 import { FISH_LIST } from "@constants/Fish.ts";
+import {
+  FISH_SORT,
+  FISH_SORT_OPTION,
+  SORT_DIRECTION,
+} from "@constants/Sort.ts";
 
 interface Props {
   setFishList: Dispatch<SetStateAction<Fish[]>>;
 }
 
 const FishListFilter = ({ setFishList }: Props) => {
+  const [sort, setSort] = useState<string>(FISH_SORT_OPTION[0].value);
   const [rank, setRank] = useState<string[]>([]);
   const [region, setRegion] = useState<string[]>([]);
   const [time, setTime] = useState<string[]>([]);
   const [keyword, setKeyword] = useState<string>("");
 
+  const sortFish = (fishList: Fish[], sort: string) => {
+    const [sortType, sortDirection] = sort.split("-");
+
+    if (sortType === FISH_SORT.RANK) {
+      if (sortDirection === SORT_DIRECTION.ASC) {
+        fishList.sort((a, b) => a.rank - b.rank);
+      } else if (sortDirection === SORT_DIRECTION.DESC) {
+        fishList.sort((a, b) => b.rank - a.rank);
+      }
+    } else if (sortType === FISH_SORT.NAME) {
+      if (sortDirection === SORT_DIRECTION.ASC) {
+        fishList.sort((a, b) => a.name.localeCompare(b.name));
+      } else if (sortDirection === SORT_DIRECTION.DESC) {
+        fishList.sort((a, b) => b.name.localeCompare(a.name));
+      }
+    }
+
+    return fishList;
+  };
+
   const filterRank = (fishList: Fish[], rank: string[]): Fish[] => {
     if (rank.length > 0) {
       return fishList.filter((fish) =>
-        rank.some((rankOption) => rankOption === fish.rank),
+        rank.some((rankOption) => rankOption === fish.rank.toString()),
       );
     } else {
       return fishList;
@@ -64,18 +90,32 @@ const FishListFilter = ({ setFishList }: Props) => {
   };
 
   useEffect(() => {
-    const fishFilterRankList = filterRank(FISH_LIST, rank);
+    const fishSortList = sortFish(FISH_LIST, sort);
+
+    const fishFilterRankList = filterRank(fishSortList, rank);
     const fishFilterRegionList = filterRegion(fishFilterRankList, region);
     const fishFilterTimeList = filterTime(fishFilterRegionList, time);
     const fishFilterKeywordList = filterKeyword(fishFilterTimeList, keyword);
 
+    console.log(fishSortList);
+    console.log(fishFilterKeywordList);
+
     setFishList(fishFilterKeywordList);
-  }, [keyword, rank, region, setFishList, time]);
+  }, [keyword, rank, region, setFishList, sort, time]);
 
   return (
     <Row style={{ marginBottom: "10px" }}>
       <Col flex={"auto"}>
         <Flex gap={"middle"}>
+          <Select
+            allowClear
+            style={{ width: "200px" }}
+            onChange={setSort}
+            options={FISH_SORT_OPTION}
+            placeholder="정렬"
+            maxTagCount={"responsive"}
+          />
+
           <Select
             mode="multiple"
             allowClear
