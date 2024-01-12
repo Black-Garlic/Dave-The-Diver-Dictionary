b5a9e3726c3fa65639e97a85719d47873e1c1237
Black-Garlic <hn1226@naver.com>
2024-01-12 18:04:34 +0900

    조미료 ID 추가

--- src/typings/Seasoning.ts
+++ src/typings/Seasoning.ts
@@ -1,10 +1,8 @@
 import { SEASONING_SOURCE } from "@constants/Seasoning.ts";
 import { Rank } from "@typings/Rank.ts";
-import { SimpleDish } from "@typings/Dish.ts";
 
 export interface Seasoning extends Rank {
   id: string;
   name: string;
   source: SEASONING_SOURCE;
-  dishList: SimpleDish[];
 }
