5a7dda6f04e9218593ac0a3695f2c42d2d859781
Black-Garlic <hn1226@naver.com>
2024-01-12 17:48:46 +0900

    요리 레시피 추가

--- src/typings/Dish.ts
+++ src/typings/Dish.ts
@@ -3,7 +3,7 @@ import { UNLOCK_TYPE } from "@constants/Dish.ts";
 export interface Dish {
   id: string;
   name: string;
-  unlockType?: UNLOCK_TYPE | string;
+  unlock?: UNLOCK_TYPE | string;
   maxCost: number;
   maxScore: number;
   maxCount: number;
