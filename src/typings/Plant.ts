7d0618163adebf42d1d79a23959d7e38e8dbe4e1
Black-Garlic <hn1226@naver.com>
2024-01-12 17:54:19 +0900

    부재료 ID 추가

--- src/typings/Plant.ts
+++ src/typings/Plant.ts
@@ -1,10 +1,8 @@
 import { PLANT_SOURCE } from "@constants/Plant.ts";
 import { Rank } from "@typings/Rank.ts";
-import { SimpleDish } from "@typings/Dish.ts";
 
 export interface Plant extends Rank {
   id: string;
   name: string;
   source: PLANT_SOURCE;
-  dishList: SimpleDish[];
 }
