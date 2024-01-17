9b9d5d3ae7b9d6ac168108fb796cf33ec8a3614f
Black-Garlic <hn1226@naver.com>
2024-01-17 20:32:24 +0900

    요리 레벨 더미 데이터 추가

---
+++ src/constants/Dummy.ts
@@ -0,0 +1,977 @@
+import { Level } from "@typings/Dish.ts";
+import { LEVEL } from "@constants/Level.ts";
+
+export const LEVEL_DUMMY: Level[] = [
+  {
+    id: "Clownfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Comber_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Cardinal_Fish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Sea_Goldie_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Pyramid_Butterflyfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Yellow_Tang_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Salema_Porgy_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Orbicular_Batfish_Fry",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Blue_Tang_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Rainbow_Wrasse_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Lagoon_Triggerfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Small_Spotted_Dart_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Yellowback_Fusilier_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Ornate_Wrasse_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Longfin_Batfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Mediterranean_Parrotfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Redtooth_Triggerfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Black_And_White_Snapper_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Green_Humphead_Parrotfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Barrel_Jellyfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Fried_Egg_Jellyfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Whitetip_Reefshark_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Moray_Eel_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Red_Lionfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Titan_Triggerfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Blacktip_Reefshark_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Copper_Shark_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Box_Jellyfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Bluefin_Tuna_Akami_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Bluefin_Tuna_Chutoro_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Bluefin_Tuna_Otoro_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Yellowfin_Tuna_Akami_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Yellowfin_Tuna_Chutoro_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Yellowfin_Tuna_Otoro_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Flame_Angelfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Sheepshead_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Emperor_Angelfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Stingray_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Marbled_Electric_Ray_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Cooked_Whiteleg_Shrimp_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Striped_Catfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Purple_Sea_Urchin_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Shortfin_Mako_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Zebra_Shark_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Marlin_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Thresher_Shark_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "American_Lobster_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "European_Lobster_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Bluehead_Tilefish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Clown_Frogfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Painted_Comber_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Bigeye_Scad_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Striped_Red_Mullet_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Mackerel_Scad_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Harleqiun_Hind_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Bigeye_Trevally_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Coral_Trout_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Grey_Triggerfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Atlantic_Bonito_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "White_Trevally_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Cuttlefish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Dusky_Grouper_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Atlantic_Mackerel_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Giant_Trevally_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "White_Spotted_Jellyfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Tiger_Shark_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Great_Barracuda_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Narrow_Barred_Spanish_Mackerel_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Longnose_Sawshark_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Atlantic_Anglerfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Sally_Lightfoot_Crab_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Raw_Black_Tiger_Shrimp_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Smooth_Hammerhead_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Sailfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "California_Spiny_Lobster_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Tropical_Rock_Lobster_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Clearfin_Lionfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Longspine_Squirrelfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "White_Shrimp_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Red_Banded_Lobster_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Blue_Lobster_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Blackfin_Barracuda_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Spear_Squid_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Humboldt_Squid_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Devil_Scorpion_Fish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Fan_Lobster_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Crystal_Lobster_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Chambered_Nautilus_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Fangtooth_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Frilled_Shark_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Bluespotted_Stargazer_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Rhinochimaeridae_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Spider_Crab_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Megamouth_Shark_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Cookiecutter_Shark_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Clione_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Sea_Toad_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Salmon_Snailfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Pacific_Fanfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Threetooth_Puffer_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Comb_Jelly_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Blood_belly_Comb_Jelly_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Red_Bream_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Norway_Lobster_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Eastern_Rock_Lobster_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Peacock_Squid_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Dumbo_Octopus_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Barreleye_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Blob_Fish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Vampire_Squid_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Pelican_Eel_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Arctic_Cod_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Gelatinous_Snailfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Antarctic_Octopus_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Greenland_Shark_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Polar_Eelpout_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Porbeagle_Shark_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Ice_Fish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Capelin_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Narwhal_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Haddock_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Starry_Skate_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Arctic_Telescope_Fish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Alaska_Pollock_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Lumpfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Snub_Nosed_Spiny_Eel_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Golden_King_Crab_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Snow_Crab_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Horsehair_Crab_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Waptia_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Pikaia_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Allenypterus_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Qingmendous_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Falcatus_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Drepanaspis_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Dunkleosteus_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Megalograptus_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Xenacanthus_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Dollocaris_Ingens_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Tokummia_Katalepsis_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Young_Anomalocaris_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Seahorse_Sewers",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Seadragon_Onigiri",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Whole_Roasted_Shark_Head",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Falcatus_Soybean_Paste_Soup",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Pikaia_Ramen",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Steamed_Hyalonema_Angler_Fish",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Hyalonema_Tuna_Sashimi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Seagrapes_Jellyfish_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Tropical_Fish_Sushi_Set",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Vegetable_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Hotpeppertuna",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Boiled_Asian_Sheepshead_Wrasse_And_Truffle",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Sweet_And_Sour_Stargazer",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Fried_Rice_With_Galapagos_Red_Crab",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Blobfish_Spring_Roll",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Tomato_Egg_Soup",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Mianbao_Xia",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Fried_Tomato_And_Snailfish",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Deep_Fish_Tempura",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Plotosid_Pie",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Pelican_Eel_Jelly",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Smoked_Atlantic_Mackerel_Scramble",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Comber_Sandwich",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Humboldt_Ink_Pasta",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Narrow_Barred_Spanish_Mackerel_Arancini",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Antarctic_Octopus_Carpaccio",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Arctic_Cod_Risotto",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Peacock_Squid_Ripieni",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Haddock_Acqua_Pazza",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Boiled_Yellowback_Fusilier",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Salt_Grilled_Redtoothed_Triggerfish",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Agar_Tokoroten",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Seasoned_Kajime",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Moray_Eel_Curry",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Striped_Red_Mullet_Tangle_Roll",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "White_Trevally_Kombu_Ochazuke",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Stellate_Puffer_Special_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Seagrapes_Special_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Trout_Sea_Grapes_Ricebowl",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Big_Eyed_Scad_And_Soybean_Paste_Roast",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Seahorse_Udon",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Black_Vinegar_Braised_Parrotfish",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Atlantic_Bonito_Curry",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Humphead_Parrotfish_Curry",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Great_Barracuda_Canape",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Nasu_Dengaku",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Dumbo_Takoyaki",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Rice_With_Great_Spider_Crab_Meat",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Bluefin_Tuna_Rice_Bowl",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Hawaiian_Poke",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Yellowfin_Tuna_Steak",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Marlin_And_Soybean_Paste_Roast",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Boiled_Sailfish_And_Seaweed",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Pickled_Vegetables",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Deep_Fried_Vegetables",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Skewered_Cucumber",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Sea_Toad_And_Cucumber_Gunkan_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Great_Spider_Crab_And_Cucumber_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Trevally_Sandwich",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Seasoned_Waptia_Fieldensis",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Stir_Fried_Habanero_Lobster",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Steamed_Eastern_Rock_Lobster_And_Egg",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Crystal_Lobster_Roll",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Stellate_Puffer_Nicogori",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Special_Fried_Shrimp_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Boiled_And_Deep_Fried_White_Shrimp",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Smallspotted_Dart_Kajime_Soup",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Fried_Seahorses",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Roasted_Capelin",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Rice_With_Purple_Sea_Urchin_Sushi",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Deep_Fried_Luna_Lionfish",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Narwhal_Miso_Soup",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Ice_Fish_Curry",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Boiled_Porbeagle_Shark",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Fried_Onion_Cuttlefish",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Three_Colored_Squid_Roast",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Latok_Omelet",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Trevally_Nanbanzuke",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Batfish_Ricebowl",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Crimson_Fish_Roll",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Dusky_Grouper_Steak",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Seahorse_Salad",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Deep_Fried_Eggplant_Shrimp_Meatballs",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Shark_Karaage",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Wrasse_Curry",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Seasoned_Jellyfish",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Great_Spider_Crab_Curry",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Dried_Stingray",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Mackerel_Scad_Hotdog",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Rice_With_White_Shrimp_Meat",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Roasted_Tropical_Fish_And_Garlic",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Seaweed_Rolled_Omelet",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Fried_Habanero_FangTooth",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Moonlight_Bladderwrack_Roll",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Secret_1",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Lobster_Platter",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Pufferfish_Dumpling_Soup",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Soy_Sauce_Marinated_Crab",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Secret_2",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Secret_3",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Secret_4",
+    level: LEVEL.ONE,
+  },
+  {
+    id: "Secret_5",
+    level: LEVEL.ONE,
+  },
+];
